16ffd4661662b17559a7071a615ba11bee192aab
feroz-hub <bashaferoz66@gmail.com>
2024-11-09 08:54:11 +0530

    Initial

---
+++ src/IntrestCalculator/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace IntrestCalculator.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
